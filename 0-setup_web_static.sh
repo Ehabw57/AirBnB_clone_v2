ba7ae38a8088313f13dbcd28ec65d71dfcf00e6c
Ehabw57 <maestor.ehab123@gmail.com>
2024-05-04 11:47:55 +0300

    ..

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,37 +3,40 @@
 # Install Nginx
 apt-get update -y
 apt-get install nginx -y
-
-# Some path variables
-test_path=/data/web_static/releases/test
-shared_path=/data/web_static/shared
+service nginx start
 
 # Create the folders if it does'nt exists
-if [ ! -d $test_path ]; then
-    mkdir -p $test_path
-fi
-if [ ! -d $shared_path ]; then
-    mkdir -p $shared_path
-fi
+mkdir -p /data/web_static/{releases/test,shared}
 
 # Create a fake HTML file
-echo "Hello Web" > $test_path/index.html
+echo "Hello Web" > /data/web_static/releases/test/index.html
 
-# Create a forces symbolic link 
-ln -sf $test_path /data/web_static/current
+# Create a forced symbolic link 
+ln -sf /data/web_static/releases/test /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
 chown -R ubuntu:ubuntu /data
 
 # Update and restart Nginx
-echo "events {
-}
-http {
-        server {
-                listen 80;
-                location /hbnb_static {
-                        alias /data/web_static/current/;
-                }
-        }
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+    }
+
+    location /redirect_me {
+        return 301 https://google.com;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        root /var/www/html;
+        internal;
+    }
 }" > /etc/nginx/nginx.conf
 nginx -s reload
