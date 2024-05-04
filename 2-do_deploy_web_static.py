643adfd446c1d0c07d1abf68748c22b77e4d12ff
Ehabw57 <maestor.ehab123@gmail.com>
2024-05-04 16:06:00 +0300

    indentaion

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,40 +13,39 @@ def do_deploy(archive_path):
         return False
 
     archive_name = archive_path[9:-4]
-    
-   result = put(local_path=archive_path, remote_path='/tmp/{}.tgz'.format(
+    result = put(local_path=archive_path, remote_path='/tmp/{}.tgz'.format(
         archive_name))
-   if result.failed:
-       return False
+    if result.failed:
+        return False
 
     run('mkdir -p  /data/web_static/releases/{}'.format(archive_name))
     result = run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.format(
         archive_name, archive_name))
-   if result.failed:
-       return False
+    if result.failed:
+        return False
 
     result = run('mv /data/web_static/releases/{}/web_static/* \
-         /data/web_static/releases/{}/'.format(archive_name, archive_name))
-   if result.failed:
-       return False
+        /data/web_static/releases/{}/'.format(archive_name, archive_name))
+    if result.failed:
+        return False
 
     result = run('rm -rf /data/web_static/releases/{}/web_static'.format(
         archive_name))
-   if result.failed:
-       return False
+    if result.failed:
+        return False
 
     result = run('rm -rf /tmp/{}.tgz'.format(archive_name))
-   if result.failed:
-       return False
+    if result.failed:
+        return False
 
     result = run('rm -rf /data/web_static/current')
-   if result.failed:
-       return False
+    if result.failed:
+        return False
 
     result = run('ln -sf /data/web_static/releases/{} \
         /data/web_static/current'.format(archive_name))
-   if result.failed:
-       return False
+    if result.failed:
+        return False
 
     print('New version deployed!')
     return True
