8d543f1aecdd3b44074bd9c334ffca3768262481
Ehabw57 <maestor.ehab123@gmail.com>
2024-08-22 22:14:00 +0300

    yeah

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A module to start a flask app on all actiove adresses"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return b'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return b'HBNB'
+
+
+@app.route('/c/<string:text>')
+def c(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<string:text>')
+@app.route('/python/')
+def python(text="is cool"):
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
